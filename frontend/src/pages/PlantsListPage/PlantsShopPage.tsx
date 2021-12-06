a7e1ff4aefbde52e33ce22c59dcf3f6872388ce3
gladys-pascual <gladyskate@gmail.com>
2021-12-06 22:19:44 +0000

    Set up fetching data in FE using axios and react-query

--- frontend/src/pages/PlantsListPage/PlantsShopPage.tsx
+++ frontend/src/pages/PlantsListPage/PlantsShopPage.tsx
@@ -1,6 +1,11 @@
 import * as React from "react";
+import { usePlants } from "../../hooks/usePlants";
 
 const PlantsShopPage = () => {
+  const { plants, plantsLoading, plantsError } = usePlants();
+  console.log(`plants`, plants);
+  console.log(`plantsLoading`, plantsLoading);
+  console.log(`plantsError`, plantsError);
   return (
     <section>
       <h1>PlantsShopPage</h1>
