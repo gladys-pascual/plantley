7424243a76053ec2d5ba945547f645207bbffc5c
gladys-pascual <gladyskate@gmail.com>
2021-12-08 19:50:33 +0000

    [FE] render plants data from API to Plants Shop Page as plant cards

--- frontend/src/components/PlantCard/PlantCard.tsx
+++ frontend/src/components/PlantCard/PlantCard.tsx
@@ -1,9 +1,38 @@
 import * as React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import "./PlantCard.css";
 
-const PlantCard = () => {
+type PlantCardProps = {
+  image: string;
+  name: string;
+  price: string;
+};
+
+const PlantCard = ({ image, name, price }: PlantCardProps) => {
   return (
-    <div>
-      <h2>plant card</h2>
+    <div className="plant-card-wrapper">
+      <Card className="plant-card" sx={{ maxWidth: 345 }}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            height="300"
+            image={`http://127.0.0.1:8000${image}`}
+            alt={name}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h6" component="h6" align="center">
+              {name}
+            </Typography>
+            <Typography variant="body2" color="text.secondary" align="center">
+              € {price}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+      </Card>
     </div>
   );
 };
---
+++ frontend/src/types.ts
@@ -0,0 +1,17 @@
+export type Plant = {
+  countInStock: number;
+  createdAt: string;
+  description: string;
+  filterByLightRequirements: string;
+  filterByPlantSize: string;
+  id: number;
+  image: string;
+  light: string;
+  name: string;
+  potSize: string;
+  price: string;
+  tips: string;
+  toxicity: string;
+  userId: number;
+  water: string;
+};
