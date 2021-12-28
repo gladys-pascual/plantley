8211e108ba30675e4ecaac345bfb5a49f0432ca7
gladys-pascual <gladyskate@gmail.com>
2021-12-28 13:04:00 +0000

    [BE] create register view and return an 400 error with customized message if email already exist

--- backend/base/views.py
+++ backend/base/views.py
@@ -14,6 +14,9 @@ from .serializers import PlantSerializer, UserSerializer, UserSerializerWithToke
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
+from django.contrib.auth.hashers import make_password
+from rest_framework import status
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
@@ -30,13 +33,22 @@ class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 
 
-@api_view(['GET'])
-def getRoutes(request):
-    routes = [
-        '/api/plants/',
-        '/api/plants/<id>/',
-    ]
-    return Response(routes)
+@api_view(['POST'])
+def registerUser(request):
+    data = request.data
+    try:
+        user = User.objects.create(
+            first_name=data['name'],
+            username=data['email'],
+            email=data['email'],
+            password=make_password(data['password'])
+        )
+
+        serializer = UserSerializerWithToken(user, many=False)
+        return Response(serializer.data)
+    except:
+        message = {'detail': 'User with this email already exists'}
+        return Response(message, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['GET'])
