7261986793bee18c16f8259096de4b7f578563b4
gladys-pascual <gladyskate@gmail.com>
2022-01-02 20:49:18 +0000

    [BE] Create addOrderItems endpoint

--- backend/base/serializers.py
+++ backend/base/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework_simplejwt.tokens import RefreshToken
-from .models import Plant
+from .models import Plant, Order, OrderItem, ShippingAddress
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -38,3 +38,43 @@ class PlantSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plant
         fields = '__all__'
+
+
+class ShippingAddressSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ShippingAddress
+        fields = '__all__'
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    orderItems = serializers.SerializerMethodField(read_only=True)
+    shippingAddress = serializers.SerializerMethodField(read_only=True)
+    user = serializers.SerializerMethodField(read_only=True)
+
+    class Meta:
+        model = Order
+        fields = '__all__'
+
+    def get_orderItems(self, obj):
+        items = obj.orderitem_set.all()
+        serializer = OrderItemSerializer(items, many=True)
+        return serializer.data
+
+    def get_shippingAddress(self, obj):
+        try:
+            address = ShippingAddressSerializer(
+                obj.shippingaddress, many=False)
+        except:
+            address = False
+        return address.data
+
+    def get_user(self, obj):
+        user = obj.userId
+        serializer = UserSerializer(user, many=False)
+        return serializer.data
---
+++ backend/base/views/order_views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+from base.models import Plant, Order, OrderItem, ShippingAddress
+from base.serializers import OrderSerializer
+
+from rest_framework import status
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def addOrderItems(request):
+    user = request.user
+    data = request.data
+
+    orderItems = data['orderItems']
+
+    if orderItems and len(orderItems) == 0:
+        return Response({'detail': 'No Order Items'}, status=status.HTTP_400_BAD_REQUEST)
+    else:
+        # 1 Create order
+        order = Order.objects.create(
+            userId=user,
+            paymentMethod=data['paymentMethod'],
+            taxPrice=data['taxPrice'],
+            shippingPrice=data['shippingPrice'],
+            totalPrice=data['totalPrice'],
+        )
+
+        # 2 Create shipping address
+        shipping = ShippingAddress.objects.create(
+            order=order,
+            address=data['shippingAddress']['address'],
+            city=data['shippingAddress']['city'],
+            postalCode=data['shippingAddress']['postalCode'],
+            country=data['shippingAddress']['country']
+        )
+
+        # 3 Create order item and set to orderItem relationship
+        for i in orderItems:
+            plant = Plant.objects.get(id=i['plantId'])
+            item = OrderItem.objects.create(
+                plant=plant,
+                order=order,
+                name=plant.name,
+                qty=i['qty'],
+                price=i['price'],
+                image=plant.image.url
+            )
+            # 4 Update stock
+            plant.countInStock -= item.qty
+            plant.save()
+
+        serializer = OrderSerializer(order, many=False)
+        return Response(serializer.data)
